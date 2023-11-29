0383916c2af52c9501e0eb7da22a142379b591fb
Mathuri <149099711+mathuri97@users.noreply.github.com>
2023-11-29 00:06:46 +0000

    Add files via upload

---
+++ SQLDataExploration.sql
@@ -0,0 +1,99 @@
+Select *
+From CovidDeaths
+Where continent is not null
+Order by 3,4
+
+Select * 
+From CovidVaccinations
+Order by 3,4
+
+Select location, date, total_cases, new_cases, total_deaths, population
+From CovidDeaths
+Where continent is not null
+Order by 1,2
+
+--Looking at Total Cases vs Total Deaths
+-- Shows the likelihood of dying if you contract COVID in the United Kingdom
+
+Select location, date, total_cases, total_deaths, (Convert(float,total_deaths)/Convert(float,total_cases))*100 as DeathPercentage
+From CovidDeaths
+where location like '%Kingdom%'
+and continent is not null
+Order by 1,2
+
+-- Looking at Total Cases vs Population 
+-- Shows what percentage of UK population got COVID
+
+Select location, date, total_cases, population, (Convert(float,total_cases)/population)*100 as PercentPopulationInfected
+From CovidDeaths
+where location like '%Kingdom%'
+and continent is not null
+Order by 1,2
+
+-- Looking at countries with highest infection rate compared to population
+
+Select location, population, MAX(total_cases) as HighestInfectionCount, Max((Convert(float,total_cases)/population))*100 as PercentPopulationInfected
+From CovidDeaths
+--where location like '%Kingdom%'
+Where continent is not null
+Group by location, population
+Order by PercentPopulationInfected desc
+
+-- Looking at Countries with highest death count per population
+
+Select location, MAX(Convert(float,total_deaths)) as TotalDeathCount
+From CovidDeaths
+--where location like '%Kingdom%'
+Where continent is not null
+Group by location
+Order by TotalDeathCount desc
+
+-- Looking at Continents with highest death count per population 
+
+Select location, MAX(Convert(float,total_deaths)) as TotalDeathCount
+From CovidDeaths
+--where location like '%Kingdom%'
+Where continent is null
+and location not like '%income%'
+Group by location 
+Order by TotalDeathCount desc
+
+-- Looking at Sum of new cases by date
+
+Select date, SUM(new_cases)as total_cases, SUM(new_deaths) as total_deaths ,(SUM(new_deaths)/ NULLIF(SUM(new_cases),0)) *100 as DeathPercentage
+From CovidDeaths
+where continent is not null
+Group by date
+Order by 1,2
+
+-- Looking at total population vs vaccinations
+
+With PopvsVac (Continent, Location, Date, Population, New_vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
+From CovidDeaths dea
+Join CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentPopulationVaccinated
+From PopvsVac
+
+
+--Creating view for to store data for later visualisation
+
+Create View PercentPopulationVaccinated as
+With PopvsVac (Continent, Location, Date, Population, New_vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.location Order by dea.location, dea.date) as RollingPeopleVaccinated
+From CovidDeaths dea
+Join CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentPopulationVaccinated
+From PopvsVac
